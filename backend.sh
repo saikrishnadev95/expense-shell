9e45771d7a751dd3c44d9ed0177e286a08432acb
saikrishnadev95 <saikrishnatsk35@gmail.com>
2023-11-08 14:11:38 +0530

    expense scripts

--- backend.sh
+++ backend.sh
@@ -24,4 +24,4 @@ systemctl start backend
 
 dnf install mysql -y
 
-mysql -h 172.31.39.165 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.saikrishna.cloud -uroot -pExpenseApp@1 < /app/schema/backend.sql
